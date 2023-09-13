49acd3e7d34d008a3fc9a099fdcc97f367c88649
misbah <misbah.khan@northsouth.edu>
2023-09-14 01:46:24 +0600

    hi

--- main.cpp
+++ main.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
-#include "UnsortedType.h"
-
+#include "unsortedtype.cpp"
 using namespace std;
 int main()
 {
-
-    UnsortedType <int> ut();
-
+    UnsortedType <int> ut;
     ut.InsertItem(5);
     ut.InsertItem(7);
     ut.InsertItem(6);
     ut.InsertItem(9);
-
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     cin >> i;
-    //     ut.InsertItem(i);
-    // }
-
-    int number;
-
-    for (int i = 0; i < ut.LengthIs(); i++)
+    int input;
+    for (int i=0; i<ut.LengthIs(); i++)
     {
-        ut.GetNextItem(number);
-        cout << number << " ";
+        ut.GetNextItem(input);
+        cout<<input<<" ";
     }
+    cout<<endl<<"The length of the list is: "<<ut.LengthIs();
+    ut.InsertItem(1);
+    ut.ResetList();
+    cout<<endl;
+    for (int i=0; i<ut.LengthIs(); i++)
+    {
+        ut.GetNextItem(input);
+        cout<<input<<" ";
+    }
+    int a=4;
+    bool f;
+    ut.RetrieveItem(a, f);
+    cout<<endl;
+    if (f==true)
+    {
+        cout<<"The item is found";
+    }
+    else{
+        cout<<"The item is not found";
+    }
+    cout<<endl;
+    if(ut.IsFull()==true)
+    {
+        cout<<"The list is full";
+    }
+    else
+    {
+        cout<<"The list is not full";
+    }
+    cout<<endl;
 
-    return 0;
+    ut.DeleteItem(6);
+    ut.ResetList();
+
+    for (int i=0; i<ut.LengthIs(); i++)
+    {
+        ut.GetNextItem(input);
+        cout<<input<<" ";
+    }
 }
